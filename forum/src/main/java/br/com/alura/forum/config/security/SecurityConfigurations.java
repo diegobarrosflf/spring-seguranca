7dfc31ec2ad4802cc40ddd51583748f910cd0159
diego <diegobarrosflf@gmail.com>
2021-06-25 19:10:43 -0300

    Documentação da API com SpringFox Swagger

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -57,7 +57,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Config de recursos estáticos
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring().antMatchers("/**.html","/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
 }
